d21673ec87044cfcc3b1d25f9b7df73482427b1b
GrandNewStart <hbjw123@naver.com>
2020-01-18 20:52:22 +0900

    Initiated a second project game, basically 'Spot the Difference' in 3D space. Currently created a sample level.

---
+++ project/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float sensitivity = 100f;
+    public GameObject playerBody = null;
+    private float xRotation = 0f;
+
+    public void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void Update()
+    {
+        Look();
+    }
+
+    void Look()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90.0f, 90.0f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.transform.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private CharacterController characterController;
+
+    public void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    public void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        float moveH = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float moveV = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        Vector3 move = transform.right * moveH + transform.forward * moveV;
+        characterController.Move(move);
+    }
+}
