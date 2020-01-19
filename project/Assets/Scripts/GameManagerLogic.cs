ea47625d8c2b71d08effccfbcdcf8bca8ed473a6
GrandNewStart <hbjw123@naver.com>
2020-01-19 22:24:37 +0900

    Created shooting machanics by raycasting. Now player can identify objects by shooting at it. Also added some post-processing effects on to the scene and the camera. The game looks more like it!

---
+++ project/Assets/Scripts/GameManagerLogic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManagerLogic : MonoBehaviour
+{
+    public float total;
+    public Text total_Text;
+
+    private void Update()
+    {
+        total_Text.text = total.ToString();
+
+        if (total == 0)
+        {
+            StageClear();
+        }
+    }
+
+    void StageClear()
+    {
+        
+    }
+}
---
+++ project/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameManagerLogic manager = null;
+    public Camera playerCamera = null;
+    public GameObject impactEffect = null;
+
+    private void Update()
+    {
+        Fire();
+    }
+
+    void Fire()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, Mathf.Infinity))
+            {
+                Debug.Log(hit.transform.tag.ToString());
+                if (hit.transform.tag == "Answer")
+                {
+                    manager.total--;
+                }
+            }
+
+            GameObject effect = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(effect, 0.5f);
+        }
+    }
+}
